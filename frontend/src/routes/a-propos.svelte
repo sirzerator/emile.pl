babf328f82bbe8e247d1b53c5ad4683f015ee003
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2022-04-16 20:09:27 -0400

    Add about page

---
+++ frontend/src/routes/a-propos.svelte
@@ -0,0 +1,44 @@
+<svelte:head>
+	<title>
+		À propos ~
+    Échos virtuels
+	</title>
+</svelte:head>
+
+<div class="tokyo_tm_about">
+	<div class="about_image">
+		<img src="/photo_wide.jpg" alt="about" />
+	</div>
+
+	<div class="description">
+		<h3 class="name">Émile Plourde-Lavoie, Développeur</h3>
+		<div class="description_inner">
+			<div class="left typography">
+				<p>
+					Bonjour! Je suis un développeur touche-à-tout, spécialisé 
+					en développement web et d'applications distribuées.
+					Je suis aussi administrateur système Linux, avec de l'expérience en gestion
+					de parcs de serveurs (<em>devops</em>, pourrait-on dire).<br>
+					Partisan de l'informatique  libre, je roule ma bosse dans les méandres du 
+					bazar numérique depuis plus de 10&nbsp;ans.
+					Entrepreneur en série, je suis aussi fondateur de quelques entreprises.
+				</p>
+
+				<div class="tokyo_tm_button">
+					<button class="ib-button">Mes projets</button>
+				</div>
+			</div>
+
+			<div class="right">
+				<dl>
+					<dt>Situation:</dt> <dd>Montréal, Québec, Canada</dd>
+					<dt>AKA:</dt> <dd>nostradamus1935, sirzerator</dd>
+					<dt>Courriel:</dt> <dd><a href="mailto:emile@echosvirtuels.com">emile@echosvirtuels.com</a></dd>
+					<dt>Téléphone:</dt> <dd><a href="tel:+15812796502">+1.581.279.6502</a></dd>
+					<dt>Études:</dt> <dd>Université du Québec à Montréal</dd>
+					<dt>Freelance:</dt> <dd><a href="#">À moyen terme</a></dd>
+				</dl>
+			</div>
+		</div>
+	</div>
+</div>
