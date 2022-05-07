00387d9d0096202c17550c1d568cfb132f3676ca
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2022-05-07 13:59:25 -0400

    Load posts in order of publication

--- frontend/src/routes/api/posts.ts
+++ frontend/src/routes/api/posts.ts
@@ -4,6 +4,7 @@ import type { EndpointOutput } from '@sveltejs/kit';
 export async function get(): Promise<EndpointOutput> {
 	const query = qs.stringify({
 		populate: 'author,image',
+		sort: 'visibleAfter',
 	});
 
 	const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts?${query}`);
