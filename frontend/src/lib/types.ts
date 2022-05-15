dc2e7d09947f9bf89febed1d69bd3056175961a5
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2022-05-15 12:00:59 -0400

    Add type for About page and localization

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,4 +1,24 @@
-export interface Post {
+type Locale = 'en' | 'fr';
+
+interface Localized {
+	locale: Locale;
+}
+
+export interface About extends Localized {
+	siteName: string;
+	slogan: string;
+	situation: string;
+	aka: string;
+	email: string;
+	telephone: string;
+	education: string;
+	availability: 'unavailable' | 'short_term' | 'medium_term' | 'long_term' | 'available';
+	bio: string;
+	created_at: string;
+	updated_at: string;
+}
+
+export interface Post extends Localized {
 	id: number;
 	title: string;
 	description: string;
