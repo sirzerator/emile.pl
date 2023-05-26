3bdd14a6dcb47ca11a6e70489446069452928cea
Émile Plourde-Lavoie <emile@echosvirtuels.com>
2023-05-26 17:21:57 -0400

    Fix Post\Published field
    Uninitialized property

--- api/app/Fields/Post/Published.php
+++ api/app/Fields/Post/Published.php
@@ -14,6 +14,7 @@ class Published extends Field implements FilterableField, ComputedField
 
     public function __construct(array &$data) {
         if (!isset($data['published'])) {
+            $this->published = null;
             return;
         }
 
