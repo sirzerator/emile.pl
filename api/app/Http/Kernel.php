bc1f3125e1e2b9eb072e2499ce806455278f6f47
Émile Plourde-Lavoie <emile@echosvirtuels.com>
2023-12-30 12:56:44 -0500

    Disable a problematic middleware
    Null !== empty string

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -6,13 +6,6 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * These middleware are run during every request to your application.
-     *
-     * @var array<int, class-string|string>
-     */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
@@ -20,7 +13,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\ConvertEmptyStringsToNullInFormsOnly::class,
         \App\Http\Middleware\WrapInTransaction::class,
     ];
 
---
+++ api/app/Http/Middleware/ConvertEmptyStringsToNullInFormsOnly.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\TransformsRequest;
+use Closure;
+
+class ConvertEmptyStringsToNullInFormsOnly extends TransformsRequest
+{
+    protected static $skipCallbacks = [];
+
+    public function handle($request, Closure $next) {
+        if ($request->isJson()) {
+            return $next($request);
+        }
+
+        foreach (static::$skipCallbacks as $callback) {
+            if ($callback($request)) {
+                return $next($request);
+            }
+        }
+
+        return parent::handle($request, $next);
+    }
+
+    protected function transform($key, $value) {
+        return $value === '' ? null : $value;
+    }
+
+    public static function skipWhen(Closure $callback) {
+        static::$skipCallbacks[] = $callback;
+    }
+}
